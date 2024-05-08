133b9faf26134b50a406a218790ed7aedbc57e8a
Jiří Vrba <jiri@vrba.dev>
2024-05-08 11:28:40 +0200

    Do not delete the template nginx conf during restarts

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,5 @@
 set -x
 set -e
 
-sed "s/\${PORT}/${PORT}/" /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf && rm /etc/nginx/nginx.conf.template
-
+sed "s/\${PORT}/${PORT}/" /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 nginx
